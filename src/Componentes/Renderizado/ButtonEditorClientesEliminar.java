191ecb17933b60bb6e9b37e81dc54798cb803165
JuanGCC <45617587+JuanGCC@users.noreply.github.com>
2021-07-25 03:38:39 -0500

    Update ButtonEditorClientesEliminar.java

--- src/Componentes/Renderizado/ButtonEditorClientesEliminar.java
+++ src/Componentes/Renderizado/ButtonEditorClientesEliminar.java
@@ -90,7 +90,7 @@ public class ButtonEditorClientesEliminar extends DefaultCellEditor {
     public  void consulta() throws SQLException {
         try{
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/examendiagnostico?serverTimezone=UTC",
-                    "root","Soyuncacahuate_1");
+                    "root","Tu_Contraseña");
             System.out.println("Conectado!");
         }catch(SQLException e){
             e.printStackTrace();
