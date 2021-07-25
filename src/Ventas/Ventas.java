b8ba244844c45f74f16dc03f42acd08f31ca7fee
JuanGCC <45617587+JuanGCC@users.noreply.github.com>
2021-07-25 03:34:45 -0500

    Update Ventas.java

--- src/Ventas/Ventas.java
+++ src/Ventas/Ventas.java
@@ -136,7 +136,7 @@ public class Ventas extends JFrame {
     public  void consulta() throws SQLException {
         try{
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/examendiagnostico?serverTimezone=UTC",
-                    "root","Soyuncacahuate_1");
+                    "root","Tu_Contraseña");
             System.out.println("Conectado!");
         }catch(SQLException e){
             e.printStackTrace();
