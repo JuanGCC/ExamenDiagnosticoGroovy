d3564a88793a4ef5a78a9d0c99546f36b7be6809
JuanGCC <45617587+JuanGCC@users.noreply.github.com>
2021-07-25 03:37:08 -0500

    Update ButtonEditorVentasProductosVendidosEliminar.java

--- src/Componentes/Renderizado/ButtonEditorVentasProductosVendidosEliminar.java
+++ src/Componentes/Renderizado/ButtonEditorVentasProductosVendidosEliminar.java
@@ -91,7 +91,7 @@ public class ButtonEditorVentasProductosVendidosEliminar extends DefaultCellEdit
     public  void consulta() throws SQLException {
         try{
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/examendiagnostico?serverTimezone=UTC",
-                    "root","Soyuncacahuate_1");
+                    "root","Tu_Contraseña");
             System.out.println("Conectado!");
         }catch(SQLException e){
             e.printStackTrace();
