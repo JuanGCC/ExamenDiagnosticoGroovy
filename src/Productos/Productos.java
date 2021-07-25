4776e7d31673d7794426c4dca6485074e078c811
JuanGCC <45617587+JuanGCC@users.noreply.github.com>
2021-07-25 03:36:23 -0500

    Update Productos.java

--- src/Productos/Productos.java
+++ src/Productos/Productos.java
@@ -144,7 +144,7 @@ public class Productos extends JFrame {
     public  void consulta() throws SQLException {
         try{
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/examendiagnostico?serverTimezone=UTC",
-                    "root","Soyuncacahuate_1");
+                    "root","Tu_Contraseña");
             System.out.println("Conectado!");
         }catch(SQLException e){
             e.printStackTrace();
